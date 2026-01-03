af30c1cad66f1929959ee8490f5b955e7b0633ec
Pierrick CAEN <pcaen@medium.com>
2026-01-03 21:22:18 +0100

    Update contact email in contact.ts to new address for improved communication.

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,6 +1,6 @@
 export const contactInfo = {
   phone: '+33 6 18 66 70 87',
-  email: 'mail@pierrickcaen.fr',
+  email: 'galerieflorence.guillot@hotmail.fr',
   airbnb: {
     blueLodge: '26058515',
     greenLodge: '1015591108415436590',
